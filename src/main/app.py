bfa57ae8479baa35946fa6b98b5d34213b76f8cc
Taishan Rowe <taishan_rowe314@outlook.com>
2021-03-22 22:15:26 +0000

    created maintenance cals calc

---
+++ src/main/app.py
@@ -0,0 +1,7 @@
+# Calculate body fat percentage => DONE
+# Calculate maintenance calories
+#   - weight, height, age input etc
+#   - dependant on sex
+# Calculate required daily macros 
+#   - dependant on weight loss or gain
+
