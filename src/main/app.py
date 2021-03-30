5eb7ced3a7e3f92bcc37f14a0de7bafcbac5ebaa
Taishan Rowe <taishan_rowe314@outlook.com>
2021-03-30 20:51:24 +0100

    began main app development

--- src/main/app.py
+++ src/main/app.py
@@ -1,3 +1,6 @@
+import time
+import os
+
 # Calculate body fat percentage => DONE
 # Calculate maintenance calories
 #   - weight, height, age input etc
@@ -5,3 +8,70 @@
 # Calculate required daily macros 
 #   - dependant on weight loss or gain
 
+
+def title_page(title):
+    with open(title, 'r') as starting_page:
+        contents = starting_page.read()
+        print(contents)
+    
+def app_initialisation(title_path):
+    clear()
+    print("initialising app .")
+    time.sleep(0.5)
+    clear()
+    print("initialising app . .")
+    time.sleep(0.5)
+    clear()
+    print("initialising app . . .")
+    time.sleep(1)
+    clear()
+    title_page(title_path)
+    time.sleep(2)
+    print("\nCreated by Taishan Rowe :)")
+    time.sleep(2)
+    clear()
+
+
+def clear():
+    # clears the screen
+    os.system("clear")
+
+def menu():
+    print(""" 
+    Welcome to Macronumeracy! 
+
+    +==========================+
+    |        MAIN MENU         |
+    +==========================+
+    | [1] Body fat percentage  |
+    | [2] Maintenance calories |
+    +==========================+
+    
+    Choose an option:
+    """)
+
+def view_another_page():  # The user decides if they wish to view the menu
+
+    while True:
+        choice = input("\nWould you like to view the menu again, Y or N?: ")
+        if choice == '' or choice == ' ':
+            continue
+        elif choice[0].lower() == 'y':
+            clear()
+            # return true
+            return choice      
+        elif choice[0].lower() == 'n':
+            clear()
+            return False                                                        
+        else:
+            print("\nI dont understand.") 
+
+
+def start():
+    app_initialisation("src/main/titlepage.txt")
+    while True:
+        clear()
+
+        menu()
+
+        
